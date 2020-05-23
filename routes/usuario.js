3c6a2bba9043b26f30036ddaa04522c92517227f
plz09 <62223358+plz09@users.noreply.github.com>
2020-05-23 13:49:47 +0100

    Registering users in the DB using hash and bcrypt
    Registering users in the DB using hash and bcrypt.

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router = expresss.Router();
 const mongoose = require('mongoose');
 require("../models/Usuario");
 const Usuario = mongoose.model("usuarios");
+const bcrypt = require("bcryptjs");
 
 router.get("/registro", (req, res) => {
     res.render("usuarios/registro")
@@ -23,11 +24,11 @@ router.post("/registro", (req, res) => {
         erros.push({texto: "Senha inválida."})
     }
 
-    if(!req.body.senha.length < 4) {
+    if(req.body.senha.length < 4) {
         erros.push({texto: "Senha muito curta."})
     }
 
-    if(!req.body.senha != req.body.senha2) {
+    if(req.body.senha != req.body.senha2) {
         erros.push({texto: "Senhas diferentes."})
     }
 
@@ -37,6 +38,42 @@ router.post("/registro", (req, res) => {
 
     }else {
         
+        Usuario.findOne({email: req.body.email}).then((usuario) => {
+            if(usuario) {
+                req.flash("error_msg", "Já existe uma conta com este email.")
+                res.redirect("/usuarios/registro")
+            }else {
+
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) => {
+                        if(erro) {
+                            res.flash("error_msg", "Ocorreu um erro no registro do usuário.")
+                            res.redirect("/")
+                        }
+
+                        novoUsuario.senha = hash
+
+                        novoUsuario.save().then(() => {
+                            req.flash("success_msg","Usuário criado com sucesso!")
+                            res.redirect("/")
+                        }).catch((err) => {
+                            req.flash("error_msg", "Ocorreu um erro ao criar usuário.")
+                            res.redirect("/usuarios/registro")
+                        })
+                    })
+                })
+
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Ocorreu um erro interno.")
+            res.redirect("/")
+        })
     }
 })
 
