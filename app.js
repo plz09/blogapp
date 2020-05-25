8f8b1851ae53d95736f919c4bad63d64bd8fcee2
plz09 <62223358+plz09@users.noreply.github.com>
2020-05-25 23:15:44 +0100

    User authentication system

--- app.js
+++ app.js
@@ -27,11 +27,15 @@
       resave: true,
       saveUninitialized: true
     }));
+
+    app.use(passport.initialize());
+    app.use(passport.session())
     app.use(flash());
   // Middleware
     app.use((req, res, next) => {
       res.locals.success_msg = req.flash("success_msg");
       res.locals.error_msg = req.flash("error_msg");
+      res.locals.error = req.flash("error");
       next();
     })
   //Body Parser
--- config/auth.js
+++ config/auth.js
@@ -8,7 +8,7 @@ const Usuario = mongoose.model("usuarios");
 
 module.exports = function(passport){
 
-    passport.use(new localStrategy({usernameField: 'email'},(email, senha, done) => {
+    passport.use(new localStrategy({usernameField: 'email', passwordField: 'senha'},(email, senha, done) => {
 
         Usuario.findOne({email: email}).then((usuario) => {
             if(!usuario){
@@ -17,7 +17,7 @@ module.exports = function(passport){
 
             bcrypt.compare(senha, usuario.senha, (erro, batem) => {
                 if(batem) {
-                    return done(null, user)
+                    return done(null, usuario)
                 }else {
                     return done(null, false, {message: "Senha incorreta"})
                 }
@@ -31,8 +31,8 @@ module.exports = function(passport){
     })
 
     passport.deserializeUser((id, done) => {
-        User.findByid(id, (err, usuario) => {
-            done(err, user)
+        Usuario.findById(id, (err, usuario) => {
+            done(err, usuario)
         })
     })
 }
